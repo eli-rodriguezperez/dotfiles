6c14fce79359f28a2e0c3141c04dcecad910fc84
eli-rodriguezperez <eli.rodriguezperez95@gmail.com>
2018-12-07 12:27:58 +0000

    Update init.el

--- init.el
+++ init.el
@@ -19,17 +19,26 @@
 (setq make-backup-files nil) ; stop creating backup~ files
 (setq auto-save-default nil) ; stop creating #autosave# files
 
+;; WINDOWS
+;; Set emacs shell to use bash
+(setq shell-file-name "C:\\msys64\\usr\\bin\\bash.exe")
+;; Set external tools for windows indexing
+(setq projectile-indexing-method (quote turbo-alien))
+;;
+
+;; WORK
 ;;set proxy
 (setq url-proxy-services '(("no_proxy" . "work\\.com")
-                           ("http" . "127.0.0.1:8081")
-						   ("https" . "127.0.0.1:8081")))
-						   
+			   ("http" . "127.0.0.1:8081")
+			   ("https" . "127.0.0.1:8081")))
+;;
+
 ;; Package configs
 (require 'package)
 (setq package-enable-at-startup nil)
 (setq package-archives '(("org"   . "http://orgmode.org/elpa/")
-                         ("gnu"   . "http://elpa.gnu.org/packages/")
-                         ("melpa" . "http://melpa.org/packages/")))
+			 ("gnu"   . "http://elpa.gnu.org/packages/")
+			 ("melpa" . "http://melpa.org/packages/")))
 (package-initialize)
 
 ;; Bootstrap `use-package`
@@ -44,6 +53,20 @@
   :config
   (load-theme 'gruvbox-dark-hard t))
 
+;; Powerline
+(use-package powerline
+  :ensure t
+  :config
+  (setq powerline-default-separator "wave")
+  )
+
+(use-package powerline-evil
+  :ensure t
+  :config
+  :init
+  (powerline-evil-center-color-theme)
+  )
+
 ;; Vim mode
 (use-package evil
   :ensure t
@@ -56,6 +79,12 @@
   (setq-default evil-escape-key-sequence "jk")
   :config
   (evil-escape-mode 1))
+;; Magit
+(use-package evil-magit
+  :ensure t)
+(use-package magit
+  :ensure t)
+
 ;; Show relative numbers
 (use-package linum-relative
   :ensure t
@@ -65,26 +94,35 @@
 ;; Only jump a single visual line when in a multiline sentence
 (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
 (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
+;; Set ñ to do the same as ;
+(define-key evil-motion-state-map "ñ" 'evil-repeat-find-char)
+
+;; Set function to change to previous buffer
+(defun er-switch-to-previous-buffer ()
+  "Switch to previously open buffer.
+Repeated invocations toggle between the two most recently open buffers."
+  (interactive)
+  (switch-to-buffer (other-buffer (current-buffer) 1)))
 
 ;; Helm
 (use-package helm
   :ensure t
   :init
   (setq helm-M-x-fuzzy-match t
-  helm-mode-fuzzy-match t
-  helm-buffers-fuzzy-matching t
-  helm-recentf-fuzzy-match t
-  helm-locate-fuzzy-match t
-  helm-semantic-fuzzy-match t
-  helm-imenu-fuzzy-match t
-  helm-completion-in-region-fuzzy-match t
-  helm-candidate-number-list 50
-  helm-split-window-in-side-p t
-  helm-move-to-line-cycle-in-source t
-  helm-echo-input-in-header-line t
-  helm-display-buffer-height 10
-  helm-autoresize-max-height 0
-  helm-autoresize-min-height 10)
+	helm-mode-fuzzy-match t
+	helm-buffers-fuzzy-matching t
+	helm-recentf-fuzzy-match t
+	helm-locate-fuzzy-match t
+	helm-semantic-fuzzy-match t
+	helm-imenu-fuzzy-match t
+	helm-completion-in-region-fuzzy-match t
+	helm-candidate-number-list 50
+	helm-split-window-in-side-p t
+	helm-move-to-line-cycle-in-source t
+	helm-echo-input-in-header-line t
+	helm-display-buffer-height 10
+	helm-autoresize-max-height 0
+	helm-autoresize-min-height 10)
   :config
   (helm-mode 1))
 
@@ -101,41 +139,49 @@
 (use-package general
   :ensure t
   :config (general-define-key
-  :states '(normal visual insert emacs)
-  :prefix "SPC"
-  :non-normal-prefix "M-SPC"
-  "TAB" '(switch-to-prev-buffer :which-key "previous buffer")
-  "SPC" '(helm-M-x :which-key "M-x")
-  ;; Company
-  "a"   '(:which-key "Company")
-  "aa"  '(company-complete :which-key "complete current word")
-  ;; Files
-  "f"   '(:which-key "File")
-  "ff"  '(helm-find-files :which-key "find files")
-  ;; Buffers
-  "b"   '(:which-key "Buffer")
-  "bb"  '(switch-to-buffer :which-key "buffers list")
-  ;; Window
-  "w"   '(:which-key "Window")
-  "wl"  '(windmove-right :which-key "move right")
-  "wh"  '(windmove-left :which-key "move left")
-  "wk"  '(windmove-up :which-key "move up")
-  "wj"  '(windmove-down :which-key "move bottom")
-  "w/"  '(split-window-right :which-key "split right")
-  "w-"  '(split-window-below :which-key "split bottom")
-  "wd"  '(delete-window :which-key "delete window")
-  ;;Projectile
-  "p"   '(:which-key "Project")
-  "pf"  '(projectile-find-file :which-key "find project file")
-  "ps"  '(projectile-switch-project :which-key "switch project")))
+	   :states '(normal visual insert emacs)
+	   :prefix "SPC"
+	   :non-normal-prefix "M-SPC"
+	   "TAB" '(er-switch-to-previous-buffer :which-key "previous buffer")
+	   "SPC" '(helm-M-x :which-key "M-x")
+	   ;; Company
+	   "a"   '(:which-key "Company")
+	   "aa"  '(company-complete :which-key "complete current word")
+	   ;; Files
+	   "f"   '(:which-key "File")
+	   "ff"  '(helm-find-files :which-key "find files")
+	   ;; Buffers
+	   "b"   '(:which-key "Buffer")
+	   "bb"  '(switch-to-buffer :which-key "buffers list")
+	   "bd"  '(evil-delete-buffer :which-key "delete current buffer")
+	   ;; Window
+	   "w"   '(:which-key "Window")
+	   "wl"  '(windmove-right :which-key "move right")
+	   "wh"  '(windmove-left :which-key "move left")
+	   "wk"  '(windmove-up :which-key "move up")
+	   "wj"  '(windmove-down :which-key "move bottom")
+	   "w/"  '(split-window-right :which-key "split right")
+	   "w-"  '(split-window-below :which-key "split bottom")
+	   "wd"  '(delete-window :which-key "delete window")
+	   ;;Projectile
+	   "p"   '(:which-key "Project")
+	   "pf"  '(counsel-projectile-find-file :which-key "find project file")
+	   "pp"  '(counsel-projectile-switch-project :which-key "switch project")
+	   "p&"  '(async-shell-command :which-key "async shell command")
+	   "p$"  '(shell-command :which-key "shell command")
+	   ;; Search
+	   "s" '(:which-key "Search")
+	   "sg"  '(counsel-projectile-git-grep :which-key "grep project")
+	   ))
 
 ;; Projectile
 (use-package projectile
   :ensure t
   :init
   :config
-  (projectile-global-mode))
-
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+  (projectile-mode 1))
 ;; Set up completion mode
 (use-package company
   :ensure t
@@ -149,6 +195,16 @@
   :ensure t)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
+
+;; PROGRAMMING LANGUAGES
+(use-package js2-mode
+  :ensure t
+  :mode "\\.js\\'"
+  :interpreter "node"
+  :init
+  (use-package json-mode
+    :ensure json-mode))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -156,7 +212,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (org-bullets ox-pandoc company projectile general which-key linum-relative helm gruvbox-theme evil-escape use-package-ensure-system-package evil))))
+    (proxy-mode counsel-projectile magit evil-magit org-bullets ox-pandoc company projectile general which-key linum-relative helm gruvbox-theme evil-escape use-package-ensure-system-package evil))))
 
 
 (custom-set-faces
